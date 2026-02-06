ee4b14f1e97cdcb9f8e00781da8282ffd958f25b
Kauã Rodrigo <kaurodrigo.joenck2@.gmail.com>
2026-02-06 05:54:35 -0300

    Fix: Renderização de dados em tela

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 async function buscarDadosResumo() {
-   let oDados = await fetch("https://script.google.com/macros/s/AKfycbz6cLjp6VDjqHR5SBVT6LLxASqBlhwaaYcynmDB6mykVEbRGVcu4L9p74dFJ3KsoFtZCg/exec?req=getResumo&tipo=Resumo");   
+   let oDados = await fetch("https://script.google.com/macros/s/AKfycbz6cLjp6VDjqHR5SBVT6LLxASqBlhwaaYcynmDB6mykVEbRGVcu4L9p74dFJ3KsoFtZCg/exec?req=getResumo&aba=Resumo");   
 
    oDados = await oDados.json();
 
-   document.getElementById("totalDespesas").textContent = `R$ ${oDados.totalDespesas}`;
-   document.getElementById("totalReceitas").textContent = `R$ ${oDados.totalReceitas}`;
-   document.getElementById("saldoFinal").textContent = `R$ ${oDados.saldoFinal}`;
+   document.getElementById("totalDespesas").textContent = `R$ ${parseFloat(oDados.totalDespesas).toFixed(2)}`;
+   document.getElementById("totalReceitas").textContent = `R$ ${parseFloat(oDados.totalReceitas).toFixed(2)}`;
+   document.getElementById("saldoFinal").textContent = `R$ ${parseFloat(oDados.saldoFinal).toFixed(2)}`;
 }
 
 $(() => {
